938332012fea544453e865c986d633fe18f2795a
w7dee <medoluxr@gmail.com>
2026-02-23 13:34:56 +0200

    Remove Eyad and Mostafa from team roster

--- src/data/team.ts
+++ src/data/team.ts
@@ -4,8 +4,6 @@ export const team: Member[] = [
   { id: "m1", name: "Ahmed Abdelnasser Ahmed", role: "Media Team" },
   { id: "m2", name: "Mohamed Gamal Hussein", role: "Leader" },
   { id: "m3", name: "Tawfiq Abdelsalam Fahmi", role: "Co-Leader" },
-  { id: "m4", name: "Eyad Hany Atef" },
   { id: "m5", name: "Ziad Mahmoud Refai" },
-  { id: "m6", name: "Mostafa Ahmed Gaber" },
   { id: "m7", name: "Ahmed Hany Abdelghani" },
 ]
